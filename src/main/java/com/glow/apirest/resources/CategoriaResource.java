e89b9f85423053b7727bb8861a89cdbb87e50ec3
joao <joao.damorim@gmail.com>
2018-10-14 00:17:03 -0300

    deploy no heroku

--- src/main/java/com/glow/apirest/resources/CategoriaResource.java
+++ src/main/java/com/glow/apirest/resources/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.glow.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,31 +16,41 @@ import org.springframework.web.bind.annotation.RestController;
 import com.glow.apirest.models.Categoria;
 import com.glow.apirest.repository.CategoriaRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Glow")
+@CrossOrigin(origins = "*")
 public class CategoriaResource {
 	
 	@Autowired
 	CategoriaRepository categoriaRepository;
 	
 	@GetMapping("/categories")
+	@ApiOperation(value="Retorna lista de categorias")
 	public List<Categoria> listaCategorias(){	
 		return categoriaRepository.findAll();	
 	}
 	@GetMapping("/categorie/{id}")
+	@ApiOperation(value="Retorna uma categoria")
 	public Categoria listaCategoriaUnico(@PathVariable(value="id") long id) {	
 		return categoriaRepository.findById(id);
 	}
 	@PostMapping("/categorie")
+	@ApiOperation(value="Salva uma categoria")
 	public Categoria salvaCategoria(@RequestBody Categoria categoria) {
 		return categoriaRepository.save(categoria);
 	}
 	@DeleteMapping("/categorie")
+	@ApiOperation(value="Deleta uma categoria")
 	public void deletaCategoria(@RequestBody Categoria categoria) {
 		categoriaRepository.delete(categoria);
 	}
 	@PutMapping("/categorie")
+	@ApiOperation(value="Atualiza uma categoria")
 	public Categoria atualizaCategoria(@RequestBody Categoria categoria) {
 		return categoriaRepository.save(categoria);
 	}
