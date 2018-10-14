893c8c8f696e2b9b3b14ce031b8a73fd89240169
João D'Amorim <joao.damorim@gmail.com>
2018-10-13 23:34:28 -0300

    [Resource] Implementação do Delete e Put.

--- src/main/java/com/glow/apirest/resources/CategoriaResource.java
+++ src/main/java/com/glow/apirest/resources/CategoriaResource.java
@@ -3,9 +3,11 @@ package com.glow.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,14 @@ public class CategoriaResource {
 	}
 	@PostMapping("/categorie")
 	public Categoria salvaCategoria(@RequestBody Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+	@DeleteMapping("/categorie")
+	public void deletaCategoria(@RequestBody Categoria categoria) {
+		categoriaRepository.delete(categoria);
+	}
+	@PutMapping("/categorie")
+	public Categoria atualizaCategoria(@RequestBody Categoria categoria) {
 		return categoriaRepository.save(categoria);
 	}
 
